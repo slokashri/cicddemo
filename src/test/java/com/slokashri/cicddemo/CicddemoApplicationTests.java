fc37742191e69fb40cd2ca117ed22a180e3857f4
Vidyasagar Vudumu <vudumu.tech@gmail.com>
2021-03-23 11:32:15 -0500

    initial version

---
+++ src/test/java/com/slokashri/cicddemo/CicddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slokashri.cicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicddemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
