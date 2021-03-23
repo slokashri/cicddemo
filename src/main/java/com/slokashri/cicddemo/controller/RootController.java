bfa69d9c9b369f873b6f39d8f78ef5a5e03b7b28
Vidyasagar Vudumu <vudumu.tech@gmail.com>
2021-03-23 12:40:44 -0500

    added root and echo endpoints

--- src/main/java/com/slokashri/cicddemo/controller/RootController.java
+++ src/main/java/com/slokashri/cicddemo/controller/RootController.java
@@ -1,4 +1,22 @@
 package com.slokashri.cicddemo.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+
+@RestController
 public class RootController {
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> helloWorld(){
+            return ResponseEntity.ok("Welcome to Slokashri's CICD Demo Application");
+    }
+
+    @GetMapping(value = "/echo",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> echo(){
+        return ResponseEntity.ok(OffsetDateTime.now(ZoneId.of("UTC")).toString());
+    }
 }
